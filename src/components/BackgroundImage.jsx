83478163e8d12b2557e849b6744c43657b8e4f45
mandrews85 <114312788+MANDREWS85@users.noreply.github.com>
2024-02-19 17:46:01 +0000

    v2

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,4 +1,11 @@
-export default function BackgroundImage({ src, title, description, link, width, height }) {
+export default function BackgroundImage({
+  src,
+  title,
+  description,
+  link,
+  width,
+  height,
+}) {
   const id = "BackgroundImage";
   return (
     <div
@@ -12,22 +19,18 @@ export default function BackgroundImage({ src, title, description, link, width, 
         width: "100%",
         height: "100%",
         margin: "auto",
+        padding: "10px",
         overflow: "hidden",
-        backgroundSize: "contain",
+
+        backgroundSize: "100%",
       }}
     >
       <div id="title-container" className="title-container">
         <a href={link} target="_blank" rel="noopener noreferrer">
           <h3>{title}</h3>
         </a>
+        <p>{description}</p>
       </div>
     </div>
   );
 }
-
-
-
-
-
-
-
--- src/components/WebsiteTemplate2.jsx
+++ src/components/WebsiteTemplate2.jsx
@@ -1,87 +1,161 @@
 import IframeContainer from "../components/IframeContainer.jsx";
 import MichiganSilverRoutingNav from "../components/MichiganSilverRoutingNav.jsx";
 import BackgroundImage from "../components/BackgroundImage.jsx";
+import ScrollingText from "../components/ScrollingText.jsx";
 import Banner from "../components/Banner.jsx";
 
 export default function WebsiteTemplate2() {
-  const id="WebsiteTemplate2";
-  return(
+  const id = "WebsiteTemplate2";
+  return (
     <div id={id} class={id}>
-
       {/*<MichiganSilverRoutingNav />*/}
       {/* https://bullion.magwebdesigns.net/wp/wp-content/uploads/2023/04/michigansilvercom-low-resolution-logo-black-on-transparent-background.png */}
 
-      <div id="Container" class="Container">
+      <div
+        id="Container"
+        class="Container"
+        style={{
+          position: "relative",
+          justifyContent: "center",
+        }}
+      >
         <div id="BannerContainer" class="BannerContainer">
           <img
             src="https://bullion.magwebdesigns.net/wp/wp-content/uploads/2023/04/michigansilvercom-low-resolution-logo-black-on-transparent-background.png"
             alt=""
             style={{
-              objectFit: 'cover',
-              width: '100%',
-              height: '100%',
-              position: 'relative',
+              objectFit: "cover",
+              width: "100%",
+              height: "100%",
+              position: "relative",
               top: 0,
               left: 0,
+              horizontalAlign: "center",
             }}
           />
-          
         </div>
       </div>
-      
+
       <div id="Container" class="Container">
         <div id="ChildContainer" class="ChildContainer">
-          <BackgroundImage 
+          <BackgroundImage
+            src=""
+            title="MichiganSilver.Com"
+            description="Welcome to MichiganSilver.com, your premier destination for high-quality silver bars and coins  in Michigan. We take pride in offering a diverse range of investment-grade silver products, including 1-ounce and gram silver coins and bars, designed to meet the needs of both seasoned investors and newcomers to the world of precious metals.
+             Our 1-ounce silver bars are meticulously crafted with precision, boasting purity and authenticity that you can trust. Whether you're looking to add to your existing collection or start a new investment journey, our 1-ounce silver coins and bars offer a convenient and tangible way to secure your financial future.
+            
+            For those seeking flexibility and affordability, our gram silver bars provide a compact yet valuable option for investment. With the same level of quality and assurance as our larger counterparts, our gram silver bars are perfect for investors looking to diversify their portfolios without breaking the bank.
+            
+            At MichiganSilver.com, we understand the importance of a seamless and secure payment process. That's why we've partnered with PayPal to handle all payment processing on our site. With PayPal, you can shop with confidence, knowing that your transactions are protected by industry-leading security measures and buyer protection policies.
+            
+            When you make a purchase on MichiganSilver.com, you'll be directed to the PayPal website to complete your transaction securely. PayPal accepts a variety of payment methods, including credit/debit cards and bank transfers, making it easy and convenient to buy silver online.
+            
+            Experience the ease and convenience of shopping for silver bars and bullion at MichiganSilver.com, where quality, reliability, and security come together to provide you with the best possible investment experience. Start building your silver portfolio today and take control of your financial future with MichiganSilver.com."
+            link="http://www.michigansilver.com"
+          />
+        </div>
+      </div>
+
+      <div style={{ maxHeight: "50px" }}>
+        <BackgroundImage title="BUY SILVER COINS" />
+      </div>
+
+      <div id="Container" class="Container">
+        <div id="ChildContainer" class="ChildContainer">
+          <BackgroundImage
+            src="https://i.ebayimg.com/images/g/u0AAAOSw~Etlwo3G/s-l1600.jpg"
+            title="Buy Eric Blood Axe Legendary Warriors 1 oz .999 Silver Coin"
+            description="Eric Blood Axe Legendary Warriors 1 oz .999 Silver Coin  Eric Bloodaxe Legendary Warriors Series 1oz .999 Silver Medallion is a SilverTowne Mint Minted Bullion Product. Located in Winchester, Indiana, The SilverTowne Mint has been manufacturing quality Silver Bullion produced with the Highest Quality Standards since 1973."
+            link="https://www.paypal.com/instantcommerce/checkout/SY46LP7KUZZM2"
+          />
+        </div>
+
+        <div id="ChildContainer" class="ChildContainer">
+          <BackgroundImage
+            src="https://i.ebayimg.com/images/g/aPwAAOSwSBlkbmt5/s-l1600.png"
+            title="Buy 1 oz .999 Silver Buffalo Round"
+            link="https://py.pl/AHkfxvedxSK"
+          />
+        </div>
+
+        <div id="ChildContainer" class="ChildContainer">
+          <BackgroundImage
+            src="https://i.ebayimg.com/images/g/yloAAOSwYMJlxqA5/s-l1600.png"
+            title="Buy 1 oz .999 Silver Buffalo Round"
+            link="https://www.ebay.com/itm/225206204790"
+          />
+        </div>
+      </div>
+
+      <div id="Container" class="Container">
+        <div id="ChildContainer" class="ChildContainer">
+          <BackgroundImage
             src="https://www.valcambi.com/uploads/tx_isunicproductdatabase/CombiBar_Ag_1x100_Retro_800_new_01.jpg"
             title="Ten 1 Gram Lot .999 Silver | Valcambi Suisse CombiBar | X 10 Barter Silver Bars"
             description="VALCAMBI SILVER BARS"
             link="https://www.ebay.com/itm/224591781336"
-            />
+          />
         </div>
 
         <div id="ChildContainer" class="ChildContainer">
-          <BackgroundImage 
+          <BackgroundImage
             src="https://www.valcambi.com/uploads/tx_isunicproductdatabase/CombiBar_Ag_1x100_Retro_800_new_01.jpg"
             title="X 25 1 Gram Lot .999 Silver | Valcambi Suisse | Twenty Five Silver Bars Mint BU"
             link="https://www.ebay.com/itm/224612277326"
-            />
+          />
         </div>
 
         <div id="ChildContainer" class="ChildContainer">
-          <BackgroundImage 
+          <BackgroundImage
             src="https://i.ebayimg.com/images/g/BNQAAOSwR9ZjSgeq/s-l500.jpg"
             title="Buy 20 Grams Total 1g .999 Bagged Silver Random Bars and Coins Nice Variety BU !!"
             link="https://www.ebay.com/itm/225206204790"
-            />
+          />
         </div>
-
       </div>
 
       <div id="Container" class="Container">
-                <div id="ChildContainer" class="ChildContainer">
-          <BackgroundImage 
+        <div id="ChildContainer" class="ChildContainer">
+          <BackgroundImage
             src="https://i.ebayimg.com/images/g/qKMAAOSwH5lhFF6P/s-l1600.jpg"
             title=" Buy 20 Grams Lot .999 Silver Bullion Bars and Coins Nice Variety BU Uncirculated!!"
             link="https://www.ebay.com/itm/225259226411"
-            />
+          />
         </div>
 
-
-
         <div id="ChildContainer" class="ChildContainer">
-          <BackgroundImage 
+          <BackgroundImage
             src="https://i.ebayimg.com/images/g/Sq4AAOSwQLJjSgeq/s-l500.jpg"
             title="Buy 10 Grams Total 1g .999 Bagged Silver Random Bars and Coins Nice Variety BU !!"
             link="https://www.ebay.com/itm/225206207869"
-            />
+          />
         </div>
-      
-      
       </div>
-    
-    </div>
-    
-  );
 
-  
+      <div id="Container" class="Container">
+        <div id="ChildContainer" class="ChildContainer">
+          <BackgroundImage
+            src=""
+            title="Welcome to MichiganSilver.com"
+            description="Your ultimate destination to buy silver in Flint, Michigan. We offer affordable silver options. Based right here in Michigan. Whether you're searching for the best prices on silver, looking to buy silver by the ounce or gram, or simply want to buy silver in Flint, MichiganSilver.com has you covered. Explore our wide range of silver products and invest with confidence. When you buy silver Michigan, through MichiganSilver.com, you're not just acquiring a shiny metal – you're making a strategic investment in your financial future. Our diverse selection includes options to buy silver by the ounce or gram, catering to investors of all budgets and preferences. Whether you're a seasoned investor looking to diversify your portfolio or a newcomer interested in exploring the world of precious metals, MichiganSilver.com is your trusted partner every step of the way. Take advantage of our user-friendly platform to buy silver in Flint, Michigan, and unlock the potential of this timeless asset. The legacy of silver mining in Michigan continues to resonate to this day, with remnants of mining towns and shafts serving as reminders of the state's rich heritage. While commercial silver mining has diminished, MichiganSilver.com offers a wide selection of silver coins and bars to suit every investor's needs.
+            The spirit of the silver rush lives on in the hearts of Michiganders."
+            link="#"
+          />
+        </div>
+
+        <div id="ChildContainer" class="ChildContainer">
+          <BackgroundImage
+            src="#"
+            title=" Why Investing in Silver Matters"
+            description="At MichiganSilver.com, we understand the importance of finding reliable avenues for investing in silver, especially in Flint, Michigan. Our platform offers a range of silver coins and bars at competitive prices, making it easier than ever to enter the precious metals market.
+
+            Why choose silver? Unlike fiat currencies that can be devalued by inflation or geopolitical events, silver has been recognized as a store of value for centuries. Its unique properties make it a versatile asset with various industrial applications, ensuring consistent demand regardless of market conditions.
+            
+            When you buy silver in Flint, Michigan, through MichiganSilver.com, you're not just acquiring a shiny metal – you're making a strategic investment in your financial future. Our diverse selection includes options to buy silver by the ounce or gram, catering to investors of all budgets and preferences. When you choose to invest in silver in Michigan through MichiganSilver.com, you're not merely procuring a commodity – you're investing in a proven asset with unwavering resilience. Our platform offers seamless options to purchase silver by the ounce or gram, catering to both seasoned investors and newcomers.  The legacy of silver mining in Michigan continues to resonate to this day, with remnants of mining towns and shafts serving as reminders of the state's rich heritage. While commercial silver mining has diminished, the spirit of the silver rush lives on in the hearts of Michiganders."
+            link="#"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
